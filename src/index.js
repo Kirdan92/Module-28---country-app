9cd9384f89a1b83d9f9a0b6d5c754b7f70690e51
Pawel Butra <pawel.butra@onet.pl>
2019-03-21 11:49:34 +0100

    Zadanie 2

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Router, hashHistory } from 'react-router';
 import { Provider } from 'react-redux';
 import store from './store';
 import DevTools from './DevTools'
+import routes from './routes';
 
 render(
 	<Provider store={store}>
 		<div>
-    		<h1>Inicjalizacja projektu</h1>
+		<Router history={hashHistory} routes={routes}>
+
+		</Router>
     		<DevTools />
 		</div>
 	</Provider>,
